ec4c80e44cb6cefbbdbd2a374cdedf058d8b05da
Santhi612 <santhimedagam@gmail.com>
2021-04-20 16:02:54 +0530

    oops file

---
+++ Introduction to c#/Oops/Oops Demo/Oops Demo/Abstraction.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Oops_Demo
+{
+    public class Abstraction
+    {
+        public string OrderNo { get; set; }
+        public string OrderValue { get; set; }
+        public string OrderCountry { get; set; }
+        public string CreateOrder()
+        {
+            validateOrderValue();
+            validateOrderCountry();
+
+            return "order Created";
+        }
+        private bool validateOrderValue()
+        {
+            //Implementation goes here
+            return true;
+        }
+        private bool validateOrderCountry()
+        {
+            return true;
+        }
+    }
+}
---
+++ Introduction to c#/Oops/Oops Demo/Oops Demo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Oops_Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Abstraction abs = new Abstraction();
+            abs.OrderNo = "101";
+            abs.OrderValue = "1000";
+            abs.OrderCountry = "India";
+            abs.CreateOrder();
+
+            Student student = new Student();
+            student.FirstName = "santhi";
+            student.LastName = "priya";
+            student.StudentAddress = "Testadress";
+            student.GetDetails();
+
+
+
+        }
+    }
+}
