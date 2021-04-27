d8823cea2846143013f46b9e869919e32034e4fe
Santhi612 <santhimedagam@gmail.com>
2021-04-27 11:52:59 +0530

    Asp.Net

---
+++ ASP.NET WEB API/Uniform Interface/TrainingCompany/TrainingCompany/Controllers/CoursesController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace TrainingCompany.Controllers
+{
+    public class CoursesController : ApiController
+    {
+        public IEnumerable<course> Get()
+        {
+            return courses; 
+        }
+        /*public course Get(int id)
+        {
+            var ret = (from c in courses
+                       where c.id == id
+                       select c).FirstOrDefault();
+            return ret;
+        }*/
+        static List<course> courses = InitCourses();
+        private static List<course> InitCourses()
+        {
+            var ret = new List<course>();
+            ret.Add(new course { id = 0, title = "WebAPI" });
+            ret.Add(new course { id = 1, title = "MobileAppliaction" });
+            return ret;
+        } 
+    }
+    public class course
+    {
+        public int id;
+        public string title;
+    }
+
+}
